da1cb6259c47cee45592e957f52d8680b330b702
Jiawei Yang <vickyoungfree@outlook.com>
2019-04-08 03:16:38 +0800

    sub

--- sock/SocketWrapperAI.py
+++
@@ -1,8 +0,0 @@
-
-
-class SocketWrapperAI:
-    def __init__(self, clientsocket, clientNumber, ai = None):
-        self.clientSocket = clientsocket
-        self.clientNumber = clientNumber
-        self.ai = ai
-        
